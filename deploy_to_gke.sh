46ab93f469f9eb26f6016dfd8b5c9c1850cf880c
Piotr Skrobski <234796@edu.p.lodz.pl>
2024-06-05 18:22:34 +0200

    Continue work on deployment script

--- deploy_to_gke.sh
+++ deploy_to_gke.sh
@@ -10,13 +10,28 @@ fi
 PROJECT_ID=$1
 ZONE=$2
 
-docker-compose up -d
+GPG_KEY=$(gpg --list-secret-keys --keyid-format SHORT | awk '/sec/{print $2}' | awk -F'/' '{print $2}')
 
-docker tag weatherapp_frontend:latest gcr.io/$PROJECT_ID/weatherapp_frontend:latest
-docker tag weatherapp_backend:latest gcr.io/$PROJECT_ID/weatherapp_backend:latest
+if [ -z "$GPG_KEY" ]; then
+    echo "No GPG key found. Generating a new one..."
+    gpg --gen-key  
+    GPG_KEY=$(gpg --list-secret-keys --keyid-format SHORT | awk '/sec/{print $2}' | awk -F'/' '{print $2}')
+fi
+
+pass init $GPG_KEY
+
+gcloud init
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://europe-north1-docker.pkg.dev
+gcloud auth configure-docker 
+gcloud config set project $PROJECT_ID
+
+docker compose up -d
+
+docker tag recruitment-efi-frontend gcr.io/$PROJECT_ID/weatherapp_frontend:latest
+docker tag recruitment-efi-frontend gcr.io/$PROJECT_ID/weatherapp_backend:latest
 
 docker push gcr.io/$PROJECT_ID/weatherapp_frontend:latest
 docker push gcr.io/$PROJECT_ID/weatherapp_backend:latest
 
-gcloud container clusters create my-cluster --num-nodes=3 --zone=$ZONE
-
+gcloud container clusters create my-cluster --num-nodes=2 --zone=$ZONE
+gcloud container clusters get-credentials my-cluster --region=$ZONE
